b0028f2e554aeb373e3ea088fcdb3edd71f8af9b
leonardsmalls <leonard.smalls37@gmail.com>
2017-08-26 05:07:54 -0500

    initial commit

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.use(express.static("./public"));
 require("./routes/html-routes.js")(app);
 require("./routes/post-api-routes.js")(app);
 require("./routes/author-api-routes.js")(app);
+require("./routes/spot-location-routes.js")(app);
 
 // Syncing our sequelize models and then starting our express app
 db.sequelize.sync({ force: true }).then(function() {
